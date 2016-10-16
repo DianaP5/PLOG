73bad1f92080813ac25dc3805bdf79d6c00d9a4b
Catarina Correia <aniratac.correia@gmail.com>
2016-10-16 13:48:51 +0100

    Adicionadas colunas e filas

--- tabuleiro_hexagonal.pl
+++ tabuleiro_hexagonal.pl
@@ -1,18 +1,18 @@
 /*Yavalath's board*/
          
 /***************        Board Representation          ******************/
-board([[s, s, s, s, s],
-       [s, s, s, s, s, s],
-       [s, s, s, s, s, s, s],
-       [s, s, s, s, s, s, s, s],
-       [s, s, s, s, s, s, s, s, s],
-       [s, s, s, s, s, s, s, s],
-       [s, s, s, s, s, s, s],
-       [s, s, s, s, s, s],
-       [s, s, s, s, s]]).
+board(  [[s, s, s, s, s],
+[s, s, s, s, s, s],
+[s, s, s, s, s, s, s],
+[s, s, s, s, s, s, s, s],
+[s, s, s, s, s, s, s, s, s],
+[s, s, s, s, s, s, s, s],
+[s, s, s, s, s, s, s],
+[s, s, s, s, s, s],
+[s, s, s, s, s]]).
 
-play_game:-board(T), display_board(T).
 
+ver:-board(T), display_board(T).
 
 
 /***************        Board Visualization          ******************/
@@ -23,44 +23,52 @@ display_board([]):-nl.
 display_board([L1|Ls]):-
         div(Ls, A, B),
         display_top_border,
-        display_top_board([L1|A]),
-        display_bottom_board(B).
+        display_top_board([L1|A], 0),
+        display_bottom_board(B, 6).
 
 /***    Display Top Half Board   ***/
-display_top_board([]).
-display_top_board([L1|Ls]):-
+
+display_top_board([], Aux).
+display_top_board([L1|Ls], Aux):-
         S_max is 9,
         length(L1, S_list),
         S is S_max-S_list,
+        write(Aux),
+        write('|'),
+        Aux1 is Aux+1,
         display_spaces(S),
         display_top_line(L1),
-        display_top_board(Ls).
+        display_top_board(Ls, Aux1).
 
 
 /***    Display Bottom Half Board   ***/
-display_bottom_board([]):-display_last_line.
-display_bottom_board([L1|Ls]):-
+display_bottom_board([], Aux):-write('9|'),
+        display_last_line.
+display_bottom_board([L1|Ls], Aux):-
         S_max is 8,
         length(L1, S_list),
         S is S_max-S_list,
+        write(Aux),
+        write('|'),
         display_spaces(S),
+        Aux1 is Aux+1,
         display_bottom_line(L1),
-        display_bottom_board(Ls).
+        display_bottom_board(Ls, Aux1).
 
 /***    Display Top Line   ***/
-display_top_line([]):- nl.
+display_top_line([], Aux):- translate(Aux, S), write(S),nl.
 
-display_top_line([E|Es]):-
+display_top_line([E|Es], Aux):-
         translate(E,V),
         write('/'),
         write(V),
         write(''\'_'),
-        display_top_line(Es).
+        display_top_line(Es, Aux).
 
 
 
 /***    Display Bottom Line   ***/
-display_bottom_line([]):- write(''\'_/'), nl.
+display_bottom_line([]):- write(''\'_/'),nl.
 
 display_bottom_line([E|Es]):-
         translate(E, V),
@@ -77,7 +85,7 @@ display_spaces(0).
 %S -> size of list
 display_spaces(S):-
         S>0,
-        write('  '),
+        write('--'),
         S1 is S-1,
         display_spaces(S1).
 
@@ -85,11 +93,13 @@ display_spaces(S):-
 
 /***    Display Top Border   ***/
 display_top_border:-
-        write('         _________________'), nl.
+        write('   A B C D E___F___G___H___I J K L M'), nl.
 
 
 /***    Display Bottom Border   ***/
-display_last_line:-write('        '\'_/_'\'_/_'\'_/_'\'_/_'\'_/').
+display_last_line:-
+        display_spaces(4),
+        write(''\'_/_'\'_/_'\'_/_'\'_/_'\'_/').
 
 
 
@@ -111,4 +121,10 @@ translate(s, ' ').
 translate(w, 'o').
 %black
 translate(b, '*').
+translate(11, 'F').
+translate(12, 'G').
+translate(13, 'H').
+translate(14, 'I').
+translate(15, 'J').
+
 
