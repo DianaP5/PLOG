e6aaa6751c65646a36bd0c0b948cf6591b4db4b9
Catarina Correia <aniratac.correia@gmail.com>
2016-11-12 02:22:26 +0000

    Added coordinates to cells

--- tabuleiro_hexagonal.pl
+++ tabuleiro_hexagonal.pl
@@ -12,6 +12,8 @@ board(  [[s, s, s, s, s],
 [s, s, s, s, s]]).
 
 
+isFree(s).
+
 play_game:-board(T), display_board(T).
 
 
@@ -27,44 +29,50 @@ display_board([L1|Ls]):-
         S is S_max-S_list,
         display_spaces(S+1),
         display_first_line(L1),
-        display_top_board([L1|A], S_max),
-        display_bottom_board(B, S_max).
+        display_top_board([L1|A], S_max, Y, 0),
+        display_bottom_board(B, S_max, Y).
 
 /***    Display Top Half Board   ***/
 
-display_top_board([Ls], S_max):- 
+display_top_board([Ls], S_max, Y, Ycount):- 
         length(Ls, S_list),
         S is S_max-S_list,
         display_spaces(S+1),
         display_top_line(Ls),
         display_spaces(S+1),
-        display_char_line_bottom(Ls),
+        X is 0, 
+        Y is Ycount+1,
+        display_char_line_bottom(Ls, X, Ycount),
         display_spaces(S+1),
         display_bottom_line(Ls).
 
 
-display_top_board([L1|Ls], S_max):-
+display_top_board([L1|Ls], S_max, Y, Ycount):-
         length(L1, S_list),
         S is S_max-S_list,
         display_spaces(S+1),
         display_top_line(L1),
         display_spaces(S),
-        display_char_line_top(L1),
-        display_top_board(Ls, S_max).
+        X is 0,
+        display_char_line_top(L1, X, Ycount),
+        Ycount1 is Ycount+1,
+        display_top_board(Ls, S_max, Y, Ycount1).
 
 
 
 /***    Display Bottom Half Board   ***/
 
-display_bottom_board([], S_max):- nl.
-display_bottom_board([L1|Ls], S_max):-
+display_bottom_board([], S_max, Y):- nl.
+display_bottom_board([L1|Ls], S_max, Y):-
         length(L1, S_list),
         S is S_max-S_list,
         display_spaces(S+1),
-        display_char_line_bottom(L1),
+        X is 0,
+        display_char_line_bottom(L1, X, Y),
+        Y1 is Y+1,
         display_spaces(S+1),
         display_bottom_line(L1),
-        display_bottom_board(Ls, S_max).
+        display_bottom_board(Ls, S_max, Y1).
 
 
 /***    Display First Line of Board  ***/
@@ -87,15 +95,16 @@ display_top_line([E|Es]):-
 
 /* Display Char line Top */
 
-display_char_line_top([]):- 
+display_char_line_top([], X, Y):- 
         write(' ___'), nl.
 
-display_char_line_top([E|Es]):-
-        translate(E, V),
-        write(' ___  '),
-        write(V),
-        write(' '),
-        display_char_line_top(Es).
+display_char_line_top([E|Es], X, Y):-
+        
+        ((isFree(E), write(' ___ '),write(X), write(','), write(Y))
+        ;
+        write(' ___  '), translate(E, V), write(V), write(' ')),
+        X1 is X+1,
+        display_char_line_top(Es, X1, Y).
 
 
 
@@ -110,18 +119,20 @@ display_bottom_line([E|Es]):-
 
 /* Display Char line Bottom */
 
-display_char_line_bottom([E]):- 
-        translate(E, V),
-        write('  '),
-        write(V),
-        write('  '), nl.
+display_char_line_bottom([E], X, Y):- 
+        
+        ((isFree(E), write(' '),write(X), write(','), write(Y))
+        ;
+        (write('  '), translate(E, V), write(V), write('  '))),
+        nl.
 
-display_char_line_bottom([E|Es]):-
-        translate(E, V),
-        write('  '),
-        write(V),
-        write('  ___'),
-        display_char_line_bottom(Es).
+display_char_line_bottom([E|Es], X, Y):-
+        ((isFree(E), write(' '),write(X), write(','), write(Y), write(' ___'))
+        ;
+        (write('  '), translate(E, V), write(V), write('  ___'))),
+        
+        X1 is X+1,
+        display_char_line_bottom(Es, X1, Y).
 
 
 
@@ -155,12 +166,9 @@ split(B, [], [], B).
 split([H|T], [_, _|T1], [H | T2], B) :-
     split(T, T1, T2, B).
 
-
 /* Translates*/
 translate(s, ' ').
 %white
 translate(w, 'o').
 %black
 translate(b, '*').
-
-
