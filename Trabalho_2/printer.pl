d560b292460c60313850712c93b9a22c5bd949a2
Catarina Correia <aniratac.correia@gmail.com>
2016-12-23 01:59:24 +0000

    almost

--- Trabalho_2/printer.pl
+++ Trabalho_2/printer.pl
@@ -1,21 +1,49 @@
 
 printBoard(Board):-
     length(Board, N),
-    printBoardTopBorder(N),
-    %printBoard(Board, 1, N),
+    printSeparator(N),
+    printBoardAux(Board, N),
     nl, !.
 
 printResultBoard(Board, Result, S):-
     length(Board, N),
-    printBoardTopBorder(N),
+    printSeparator(N),
     %printBoard(Board, 1, N, Result, S),
     nl, !.
 
 
-printBoardTopBorder(N):-
+printSeparator(N):-
     write('    +'),
-    createSeparator(N, '------+'), nl.
+    createSeparator(N, '-------+'), nl.
 
+%print empty board
+printBoardAux([], _).
+
+printBoardAux([Row|Board], N):-
+    write('    |'),
+    createSeparator(N, '       |'), nl,
+    write('    |'),
+    printRow(Row), nl,!,
+    write('    |'),
+    createSeparator(N, '       |'), nl,
+    printSeparator(N),
+    printBoardAux(Board, N).
+
+/*********************************************************************************************************/
+
+printRow([]).
+
+printRow([H|Row]):-
+    integer(H),
+    write('   '),
+    write(H),
+    write('   |'),
+    printRow(Row).
+
+printRow([H|Row]):-
+    var(H),
+    write('       |'),
+    printRow(Row).
 
 /*********************************************************************************************************/
 
@@ -25,9 +53,6 @@ createSeparator(N, SS):-
 	write(SS),
 	N1 is N - 1,
 	createSeparator(N1, SS), !.
-
-/*********************************************************************************************************/
-
 
 /*********************************************************************************************************/
 
